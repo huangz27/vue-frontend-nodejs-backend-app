bb2729ce87d16283445e5cef3b17527eabfc1607
huangz27 <huangz27@hotmail.com>
2020-06-10 09:49:55 +0000

    edit vue.config.js

--- vue-frontend/vue.config.js
+++ vue-frontend/vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-   devServer: {
-	// host: '165.22.99.104',
-	disableHostCheck: true,
-        port: 8080,
+ 
+    devServer: {
+	//host: '165.22.99.104',
+        public: '165.22.99.104:8081',
+        disableHostCheck: true,
+        //port: 8080,
         https: true,
     }
+
 }
